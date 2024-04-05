fbde90d6976485cd324e19a44119e4d762e19fcb
codejutsu1 <codejutsu@protonmail.com>
2024-04-05 06:44:04 +0100

    worked on the configureation

--- config/paystack-transfer.php
+++ config/paystack-transfer.php
@@ -1,6 +1,17 @@
 <?php
 
-// config for Codejutsu1/LaravelPaystackTransfer/PaystackTransfer
+// config for Codejutsu1/LaravelPaystackTransfer
 return [
+     /**
+     * Public Key From Paystack Dashboard
+     *
+     */
+    'public_key' => env('PAYSTACK_PUBLIC_KEY'),
+
+    /**
+     * Secret Key From Paystack Dashboard
+     *
+     */
+    'secret_key' => env('PAYSTACK_SECRET_KEY'),
 
 ];
--- src/PaystackTransfer.php
+++ src/PaystackTransfer.php
@@ -4,4 +4,23 @@ namespace Codejutsu1\LaravelPaystackTransfer;
 
 class PaystackTransfer
 {
+    protected ?string $secret_key;
+
+    protected ?string $public_key;
+
+    public function __construct()
+    {
+        $this->setKeys();
+    }
+
+    protected function setKeys()
+    {
+        $this->secret_key = config('paystack.secretKey');
+        $this->public_key = config('paystack.publicKey');
+    }
+
+    public function seeConfigKeys()
+    {
+        return env("PAYSTACK_PUBLIC_KEY");
+    }
 }
