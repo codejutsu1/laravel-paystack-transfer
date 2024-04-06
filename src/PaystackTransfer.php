47e35e267b707fe2666df948452cac7c2eec9948
codejutsu1 <codejutsu@protonmail.com>
2024-04-06 21:58:10 +0100

    added the transfer methods

--- src/PaystackTransfer.php
+++ src/PaystackTransfer.php
@@ -13,9 +13,16 @@ class PaystackTransfer
 
     protected object $response;
 
+    protected array $transfer_parameters;
+
     public function __construct()
     {
         $this->setKeys();
+
+        $this->transfer_parameters = [
+            "currency" => "NGN",
+            "source" => "balance"
+        ];
     }
 
     protected function setKeys()
@@ -24,11 +31,18 @@ class PaystackTransfer
         $this->public_key = config('paystack-transfer.public_key');
     }
 
-    public function createTransferRecipient(array $para): array
+    public function createTransferRecipient(array $parameters): array
     {
         //$data = ['type', 'name', 'account_number', 'bank_code', 'currency'];
         return Http::withToken($this->secret_key)
-                    ->post("https://api.paystack.co/transferrecipient", $para)
+                    ->post("https://api.paystack.co/transferrecipient", $parameters)
+                    ->json();
+    }
+
+    public function bulkTransferRecipient(array $parameters): array
+    {
+        return Http::withToken($this->secret_key)
+                    ->post("https://api.paystack.co/transferrecipient/bulk", $parameters)
                     ->json();
     }
 
@@ -77,6 +91,53 @@ class PaystackTransfer
     {
         return Http::withToken($this->secret_key)
                     ->get("https://api.paystack.co/bank/resolve", $queryParameters)
+                    ->json();
+    }
+
+    public function singleTransfer(array $parameters): array 
+    {
+        return Http::withToken($this->secret_key)
+                    ->post("https://api.paystack.co/transfer", $parameters)
+                    ->json();
+    }
+
+    /**
+     * OTP Enabled
+     */
+    public function finalizeTransfer(array $parameters): array
+    {
+        return Http::withToken($this->secret_key)
+                    ->post("https://api.paystack.co/transfer/finalize_transfer", $parameters)
+                    ->json();
+    }
+
+    public function bulkTransfer(array $transfers): array
+    {
+        $data = array_merge($this->transfer_parameters, $transfers);
+
+        return Http::withToken($this->secret_key)
+                    ->post("https://api.paystack.co/transfer/bulk", $data)
+                    ->json();
+    }
+
+    public function listTransfers(array $queryParameters): array
+    {
+        return Http::withToken($this->secret_key)
+                    ->get("https://api.paystack.co/transfer", $queryParameters)
+                    ->json();
+    }
+
+    public function fetchTransfer(int|string $id_or_code): array
+    {
+        return Http::withToken($this->secret_key)
+                    ->get("https://api.paystack.co/transfer/{$id_or_code}")
+                    ->json();
+    }
+
+    public function verifyTransfer(string $reference): array
+    {
+        return Http::withToken($this->secret_key)
+                    ->get("https://api.paystack.co/transfer/verify/{$reference}")
                     ->json();
     }
 
